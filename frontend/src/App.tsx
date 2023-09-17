22ec6525539428fdc106fdc6c3478af6e42e793e
Alfredo Ochoa López <ochoalopezalfredo@gmail.com>
2023-09-16 18:18:35 -0600

    Save files after moved from apps folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  // form inputs , width, height,youngKeanu,grayscale
+  return (
+    <>SOME CHANGE</>
+  );
+}
+
+export default App;
