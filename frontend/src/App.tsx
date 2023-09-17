b31384d7fd6ba2404cf10917522e5af266eac317
Alfredo Ochoa López <ochoalopezalfredo@gmail.com>
2023-09-17 17:56:26 -0600

    Added form and styles to main component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
 
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { ImageComponent } from './components/image';
+import { fetchImageRequest } from './redux/actions';
+import { ImageOptions, RootState } from './types';
 
-function App() {
-  // form inputs , width, height,youngKeanu,grayscale
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { data, error, loading } = useSelector((state: RootState) => state.image)
+  const { register, handleSubmit, formState: { isValid }, } = useForm<ImageOptions>()
+
+
+
+  const onSubmit = (values: ImageOptions) => {
+    values.width = +values.width
+    if (values.height) {
+      values.height = +values.height
+    }
+    dispatch(fetchImageRequest(values))
+  }
+
+
   return (
-    <>SOME CHANGE</>
-  );
+    <div className='container'>
+      <form className='form' onSubmit={handleSubmit(onSubmit)}>
+        <label>Width</label>
+        <input type='number'  {...register("width", { required: true })} />
+        <label>Height</label>
+        <input type='number'  {...register("height")} />
+        <label>Grayscale: <input type="checkbox"   {...register("grayscale")} /> </label>
+        <label>Young:   <input type="checkbox"  {...register("young")} /></label>
+
+        <input disabled={!isValid} type="submit" value={'Generate image'} />
+      </form>
+      {loading && <p>LOADING IMAGE ...</p>}
+      {!loading && <ImageComponent data={data} error={error} />}
+    </div>
+  )
+
 }
 
-export default App;
+
+export default App
