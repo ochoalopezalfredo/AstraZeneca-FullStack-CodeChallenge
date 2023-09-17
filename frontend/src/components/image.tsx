9be1c3e679ba06f448f8a4b4f38e289f15482097
Alfredo Ochoa López <ochoalopezalfredo@gmail.com>
2023-09-17 17:53:57 -0600

    Refactor component to allow ui states

--- frontend/src/components/image.tsx
+++ frontend/src/components/image.tsx
@@ -1,27 +1,15 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchImageRequest } from "../redux/actions";
 
-import { ImageOptions } from "../types";
-import { RootState } from "../redux/reducers";
-
-
-export const ImageComponent = ({ ...props }: ImageOptions) => {
-    const { data, error } = useSelector((state: RootState) => state.image)
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchImageRequest(props))
-    }, [])
-
+import { ImageState } from "../types";
+import '../App.css'
+export const ImageComponent = ({ error, data }: Omit<ImageState, "loading">) => {
 
     return (
-        <div>
+        <div className="image-viewer">
             {error && <div>Error: {error}</div>}
             {data && (
                 <>
                     <img src={data.image} alt="Keanu Reaves" />
-                    <p>{data.url}</p>
+                    <p>Loaded from: {data.url}</p>
                 </>)}
         </div>
     );
