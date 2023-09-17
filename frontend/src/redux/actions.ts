d38fe1c04d6ff8c49ddba82bd34b9386c7f31ae9
Alfredo Ochoa López <ochoalopezalfredo@gmail.com>
2023-09-17 17:54:31 -0600

    Added loading state and rename parameter

--- frontend/src/redux/actions.ts
+++ frontend/src/redux/actions.ts
@@ -2,6 +2,7 @@ import { ImageOptions } from "../types";
 
 export const FETCH_IMAGE_REQUEST = 'FETCH_IMAGE_REQUEST';
 export const FETCH_IMAGE_SUCCESS = 'FETCH_IMAGE_SUCCESS';
+export const FETCH_IMAGE_LOADING = 'FETCH_IMAGE_LOADING';
 export const FETCH_IMAGE_FAILURE = 'FETCH_IMAGE_FAILURE';
 
 
@@ -17,14 +18,19 @@ interface FetchImageSuccessAction {
     type: typeof FETCH_IMAGE_SUCCESS;
     payload: ImageResponse;
 }
+interface FetchImageLoadingAction {
+    type: typeof FETCH_IMAGE_LOADING;
+    payload: boolean;
+}
 
 interface FetchImageFailureAction {
     type: typeof FETCH_IMAGE_FAILURE;
     payload: string;
 }
 
-export type ImageActionTypes = FetchImageRequestAction | FetchImageSuccessAction | FetchImageFailureAction
+export type ImageActionTypes = FetchImageRequestAction | FetchImageSuccessAction | FetchImageFailureAction | FetchImageLoadingAction
 
-export const fetchImageRequest = (params: ImageOptions): FetchImageRequestAction => ({ type: FETCH_IMAGE_REQUEST, payload: params });
-export const fetchImageSuccess = (data: ImageResponse): FetchImageSuccessAction => ({ type: FETCH_IMAGE_SUCCESS, payload: data });
-export const fetchImageFailure = (error: string): FetchImageFailureAction => ({ type: FETCH_IMAGE_FAILURE, payload: error });
+export const fetchImageRequest = (payload: ImageOptions): FetchImageRequestAction => ({ type: FETCH_IMAGE_REQUEST, payload });
+export const fetchImageSuccess = (payload: ImageResponse): FetchImageSuccessAction => ({ type: FETCH_IMAGE_SUCCESS, payload });
+export const fetchImageLoading = (payload: boolean): FetchImageLoadingAction => ({ type: FETCH_IMAGE_LOADING, payload });
+export const fetchImageFailure = (payload: string): FetchImageFailureAction => ({ type: FETCH_IMAGE_FAILURE, payload });
