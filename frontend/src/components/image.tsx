2434f78ddf22b34bc5b3aab5f3067cebf9515660
Alfredo Ochoa López <ochoalopezalfredo@gmail.com>
2023-09-17 22:11:20 -0600

    Convert image component to Chakra UI

--- frontend/src/components/image.tsx
+++ frontend/src/components/image.tsx
@@ -1,16 +1,34 @@
 
 import { ImageState } from "../types";
 import '../App.css'
-export const ImageComponent = ({ error, data }: Omit<ImageState, "loading">) => {
+import { Box, Card, Image, Spinner, Text } from "@chakra-ui/react";
+export const ImageComponent = ({ error, data, loading }: ImageState) => {
 
     return (
-        <div className="image-viewer">
-            {error && <div>Error: {error}</div>}
-            {data && (
-                <>
-                    <img src={data.image} alt="Keanu Reaves" />
-                    <p>Loaded from: {data.url}</p>
-                </>)}
-        </div>
+        <Box mt={10} >
+            {error && <Text size={'md'}  >Error: {error}</Text>}
+            {loading &&
+                <ImageContainer>
+                    <Spinner
+                        thickness='4px'
+                        speed='0.65s'
+                        emptyColor='gray.200'
+                        color='blue.500'
+                        size='xl'
+                    />
+                </ImageContainer>
+            }
+            {data && !loading && (
+                <ImageContainer>
+                    <Image objectFit={'contain'} src={data.image} alt='Keanu Reeves' />
+                    <Text size={'sm'} color={'dimgray'} >Generated from: {data.url}</Text>
+                </ImageContainer>
+            )}
+        </Box >
     );
 };
+
+const ImageContainer = ({ children }: { children: JSX.Element | JSX.Element[] }) =>
+    <Card minH={80} p={20} justifyContent={'center'} alignItems={'center'}>
+        {children}
+    </Card>
