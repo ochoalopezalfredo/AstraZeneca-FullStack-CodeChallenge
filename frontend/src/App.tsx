098ffdc481bb2d3985da97fb82ca27449734c688
Alfredo Ochoa López <ochoalopezalfredo@gmail.com>
2023-09-17 22:15:31 -0600

    Convert form app  to Chakra UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,156 @@
 
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
-import './App.css';
 import { ImageComponent } from './components/image';
 import { fetchImageRequest } from './redux/actions';
 import { ImageOptions, RootState } from './types';
+import { Box, Button, Card, CardHeader, Center, Flex, FormControl, FormHelperText, FormLabel, Heading, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Spinner, Switch } from '@chakra-ui/react'
+import { Divider } from '@chakra-ui/react'
 
+type FormData = {
+  width: number,
+  height?: number,
+  grayscale: number,
+  young: number,
+  square: number
+}
 
 const App = () => {
   const dispatch = useDispatch();
   const { data, error, loading } = useSelector((state: RootState) => state.image)
-  const { register, handleSubmit, formState: { isValid }, } = useForm<ImageOptions>()
+  const { watch, control, handleSubmit, formState: { isValid, errors } } = useForm<FormData>()
 
+  const squareImage = watch('square');
 
-
-  const onSubmit = (values: ImageOptions) => {
-    values.width = +values.width
-    values.height = Number(values?.height) ?? values.width
-    dispatch(fetchImageRequest(values))
+  const onSubmit = ({ square, ...values }: FormData) => {
+    const params = values as unknown as ImageOptions
+    if (squareImage) {
+      params.height = undefined
+    }
+    dispatch(fetchImageRequest(params))
   }
 
-
   return (
-    <div className='container'>
-      <form className='form' onSubmit={handleSubmit(onSubmit)}>
-        <label>Width</label>
-        <input type='number'  {...register("width", { required: true })} />
-        <label>Height</label>
-        <input type='number'  {...register("height")} />
-        <label>Grayscale: <input type="checkbox"   {...register("grayscale")} /> </label>
-        <label>Young:   <input type="checkbox"  {...register("young")} /></label>
+    <Center>
+      <Box p={50} w={'80%'} >
+        <Card py={10} px={20}>
+          <CardHeader px={0}>
+            <Heading >Keanu Reeves image generator</Heading>
+          </CardHeader>
+          <Controller
+            name="width"
+            control={control}
+            rules={{
+              required: true,
+              max: 1000,
+              min: 1
+            }}
+            render={({ field }) =>
+              <FormControl>
+                <FormLabel>Width</FormLabel>
+                <Flex>
+                  <NumberInput maxW={1000} mr={10} {...field}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                  <Slider
+                    max={1000}
+                    flex='1'
+                    focusThumbOnChange={false}
+                    {...field}
+                  >
+                    <SliderTrack>
+                      <SliderFilledTrack bg='blue.500' />
+                    </SliderTrack>
+                    <SliderThumb bg='blue.500' fontSize='sm' boxSize='32px' />
+                  </Slider>
+                </Flex>
+                <FormHelperText>{errors.width?.message}</FormHelperText>
+              </FormControl>
+            }
+          />
 
-        <input disabled={!isValid} type="submit" value={'Generate image'} />
-      </form>
-      {loading && <p>LOADING IMAGE ...</p>}
-      {!loading && <ImageComponent data={data} error={error} />}
-    </div>
+          {!squareImage && <Controller
+            name="height"
+            control={control}
+            rules={{
+              required: !!!squareImage,
+              max: 1000,
+              min: 1
+            }}
+            render={({ field }) =>
+              <FormControl >
+                <FormLabel>Height</FormLabel>
+                <Flex>
+                  <NumberInput maxW={1000} mr={10} {...field}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                  <Slider
+
+                    max={1000}
+                    flex='1'
+                    focusThumbOnChange={false}
+                    {...field}
+                  >
+                    <SliderTrack>
+                      <SliderFilledTrack bg='blue.500' />
+                    </SliderTrack>
+                    <SliderThumb bg='blue.500' fontSize='sm' boxSize='32px' />
+                  </Slider>
+                </Flex>
+                <FormHelperText>{errors.width?.message}</FormHelperText>
+              </FormControl>
+            }
+          />}
+          <Controller
+            name="square"
+            control={control}
+            render={({ field }) =>
+              <FormControl>
+                <Flex alignItems={'center'}>
+                  <FormLabel m={0} mr={10}>Square image</FormLabel>
+                  <Switch   {...field} />
+                </Flex>
+              </FormControl>
+            }
+          />
+          <Controller
+            name="grayscale"
+            control={control}
+            render={({ field }) =>
+              <FormControl>
+                <Flex alignItems={'center'}>
+                  <FormLabel m={0} mr={10}>Grayscale</FormLabel>
+                  <Switch   {...field} />
+                </Flex>
+              </FormControl>
+            }
+          />
+          <Controller
+            name="young"
+            control={control}
+            render={({ field }) =>
+              <FormControl>
+                <Flex alignItems={'center'}>
+                  <FormLabel m={0} mr={16}>Young </FormLabel>
+                  <Switch  {...field} />
+                </Flex>
+              </FormControl>}
+          />
+          <Button mt={20} colorScheme='blue' isDisabled={!isValid} onClick={handleSubmit(onSubmit)} >Generate imagen</Button>
+        </Card>
+        <Divider />
+
+        <ImageComponent data={data} error={error} loading={loading} />
+      </Box>
+    </Center>
   )
 
 }
